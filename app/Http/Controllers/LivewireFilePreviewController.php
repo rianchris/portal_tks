23ea4cadc19d0ce141d5d2b16663baca33145a56
Chris <chrissesario.rian@gmail.com>
2026-02-28 16:32:58 +0700

    perbaiki

--- app/Http/Controllers/LivewireFilePreviewController.php
+++ app/Http/Controllers/LivewireFilePreviewController.php
@@ -1,19 +1,14 @@
-
 <?php
 
 namespace App\Http\Controllers;
 
-use Livewire\Features\SupportFileUploads\FileUploadConfiguration;
 use Livewire\Drawer\Utils;
 
 class LivewireFilePreviewController extends Controller
 {
     public function handle($filename)
     {
-        // Skip signature validation karena Hostinger memotong query string
-        // Ganti dengan session auth check
         abort_unless(auth()->check(), 401);
-
-        return Utils::pretendResponseIsFile($filename);
+        return Utils::pretendPreviewResponseIsPreviewFile($filename);
     }
 }
